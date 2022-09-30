52e44ed0cf697c53578f19fd3c3bbae047368a09
Junell777 <janejunell777@gmail.com>
2022-09-30 17:03:23 +0300

    updated index.html

--- index.php
+++ index.php
@@ -5,4 +5,17 @@ $database = new Database;
 
 $database->query('SELECT * FROM table_name');
 $rows = $database->resultset();
-print_r($rows);
+
+/*print_r($rows); //prints as arrays*/
+?>
+
+<h1>Posts</h1>
+<div>
+<?php foreach($rows as $row) : ?>
+	<div>
+		<h3><?php echo $row['title']; ?></h3>  //title is a column from our table containing titles
+		<p><?php echo $row['body']; ?></p>    //body is a column from our table containing blog body
+	</div>
+<?php endforeach; ?>
+</div>
+
