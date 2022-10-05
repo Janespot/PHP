6206780b5e1efb18c47c2a9b66b453382059ce2e
Junell777 <janejunell777@gmail.com>
2022-10-05 10:11:19 +0300

    updated update.php

--- update.php
+++ update.php
@@ -42,6 +42,8 @@ $rows = $database->resultset();
 <div>
 <?php foreach($rows as $row) : ?>
         <div>
-                <h3><?php echo $row['title']; ?></h3>  <!--title is a column from our table containing titles-->
-		<p><?php echo $row['body']; ?></p>    <!--body is a column from our table containing blog body-->
-																		   2,32
+                <h3><?php echo $row['title']; ?></h3> 
+		<p><?php echo $row['body']; ?></p>    
+	</div>
+<?php endforeach; ?>
+</div>
