10f5d4a309ea40f915317395471c5108b89d4bf0
Junell777 <janejunell777@gmail.com>
2022-10-05 13:22:48 +0300

    Bootstrap.php

---
+++ MyPHP_Project/classes/Bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+class Bootstrap{
+	private $controller;
+	private $action;
+	private $request;
+
+	public function __construct($request){
+		$this->request = $request;
+		if($this->request['controller'] == ""){
+
+		}
+	}
+}
+
